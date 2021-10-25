5be6484e3c19b67a129f295458cf6b9710d28aad
André <andre.gmaps@gmail.com>
2021-10-25 20:31:46 -0300

    refatorando service

--- src/main/java/br/com/project/api/model/Product.java
+++ src/main/java/br/com/project/api/model/Product.java
@@ -1,23 +1,25 @@
 package br.com.project.api.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.hateoas.RepresentationModel;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "products")
-public class Product extends RepresentationModel<Product> {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	@NotNull
-	private String name;
-	@NotNull
-	private BigDecimal price;
-	
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false)
+    private BigDecimal price;
+
 }
--- src/main/java/br/com/project/api/service/ProductService.java
+++ src/main/java/br/com/project/api/service/ProductService.java
@@ -1,76 +1,82 @@
 package br.com.project.api.service;
 
-import br.com.project.api.controller.ProductController;
-import br.com.project.api.model.Product;
+import br.com.project.api.controller.ProductsController;
+import br.com.project.api.dto.ProductDto;
+import br.com.project.api.mapper.ProductMapper;
 import br.com.project.api.repository.ProductRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
-@AllArgsConstructor
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RequiredArgsConstructor
 @Service
 public class ProductService {
 
-	private ProductRepository productRepository;
-	
-	public ResponseEntity<List<Product>> findAllProducts() {
-		List<Product> productList = productRepository.findAll();
-		
-		if(productList.isEmpty()) {
-			return ResponseEntity.notFound().build();
-		} else {
-			for (Product product : productList) {
-				product.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
-						.methodOn(ProductController.class).returnsProductById(product.getId()))
-						.withSelfRel());
-			}
-		}
-		return ResponseEntity.ok(productList);
-	}
-	
-	public ResponseEntity<Product> findProductById(Long id) {
-		Optional<Product> product = productRepository.findById(id);
-		
-		if(!product.isPresent())
-			return ResponseEntity.notFound().build();
-		else 
-			product.get().add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
-					.methodOn(ProductController.class).returnsAllProducts())
-					.withRel("Product list"));
-			
-		return ResponseEntity.ok(product.get());
-	}
-	
-	public ResponseEntity<Product> save(Product product) throws URISyntaxException {
-		return ResponseEntity.created(new URI("/products")).body(productRepository.save(product));
-	}
-	
-	public ResponseEntity<Product> remove(Long id) {
-		Optional<Product> product = productRepository.findById(id);
-		
-		if(!product.isPresent()) {
-			return ResponseEntity.notFound().build();
-		} else {
-			productRepository.delete(product.get());
-			return ResponseEntity.ok().build();
-		}
-	}
-	
-	public ResponseEntity<Product> update(Long id, Product product) {
-		Optional<Product> productFound = productRepository.findById(id);
-		
-		if(!productFound.isPresent()) {
-			return ResponseEntity.notFound().build();
-		} else {
-			product.setId(productFound.get().getId());
-			return ResponseEntity.ok().body(productRepository.save(product));
-		}
-	}
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper = ProductMapper.INSTANCE;
+
+    public ResponseEntity<List<ProductDto>> findAll() {
+        var products = productRepository.findAll();
+
+        return products.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(products
+                .stream()
+                .map(productMapper::toDto)
+                .map(ProductService::addSelfLinkRelation)
+                .collect(Collectors.toList()));
+    }
+
+    private static ProductDto addSelfLinkRelation(ProductDto productDto) {
+        return productDto.add(linkTo(methodOn(ProductsController.class).findById(productDto.getId())).withSelfRel());
+    }
+
+    public ResponseEntity<ProductDto> findById(Long id) {
+        var productFound = productRepository.findById(id);
+
+        return productFound.isEmpty() ? ResponseEntity.notFound().build() :
+                ResponseEntity.ok(addLinkRelation(productMapper.toDto(productFound.get())));
+    }
+
+    private static ProductDto addLinkRelation(ProductDto productDto) {
+        return productDto.add(linkTo(methodOn(ProductsController.class).findAll()).withRel("Product list"));
+    }
+
+    public ResponseEntity<ProductDto> save(ProductDto productDto) throws URISyntaxException {
+        var product = productRepository.save(productMapper.toModel(productDto));
+
+        return ResponseEntity.created(new URI("/products"))
+                .body(productMapper.toDto(product));
+    }
+
+    public ResponseEntity remove(Long id) {
+        var productFound = productRepository.findById(id);
+
+        if (productFound.isPresent()) {
+            productRepository.delete(productFound.get());
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    public ResponseEntity<ProductDto> update(Long id, @Valid ProductDto productDto) {
+        var productFound = productRepository.findById(id);
+
+        if (productFound.isPresent()) {
+            productDto.setId(productFound.get().getId());
+            var product = productRepository.save(productMapper.toModel(productDto));
+            return ResponseEntity.ok().body(productMapper.toDto(product));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 
 }
